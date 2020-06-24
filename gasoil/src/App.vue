5ef7cf9ccae4c04a88ec2813ab78374db90908e7
clabroche <corentin.labroche@sailendra.fr>
2020-06-24 16:20:55 +0200

    fix(scrollbar): change design

--- gasoil/src/App.vue
+++ gasoil/src/App.vue
@@ -12,4 +12,22 @@ body {
   font-family: Jost, sans-serif;
   margin:0
 }
+::-webkit-scrollbar {
+  width: 10px;
+}
+
+/* Track */
+::-webkit-scrollbar-track {
+  background: #f1f1f100;
+}
+
+/* Handle */
+::-webkit-scrollbar-thumb {
+  background: #888;
+}
+
+/* Handle on hover */
+::-webkit-scrollbar-thumb:hover {
+  background: #555;
+}
 </style>
