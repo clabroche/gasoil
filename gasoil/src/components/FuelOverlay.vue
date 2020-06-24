94bec20f0032e39e3c0c41948889047d7bc8299d
clabroche <corentin.labroche@sailendra.fr>
2020-06-24 15:59:18 +0200

    fix(refactoring): put fuel overlay in it own vue file

---
+++ gasoil/src/components/FuelOverlay.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="overlay">
+      <div>Type de carburant:</div>
+      <select :value="Stations.carburant" @input="Stations.setCarburant($event.target.value)">
+        <option :value="carburant.id" v-for="carburant of Stations.carburants" :key="carburant.id">{{carburant.label}}</option>
+      </select>
+    </div>
+</template>
+
+<script>
+import Stations from '../services/stations'
+export default {
+  data() {
+    return {
+      Stations
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.overlay {
+  position: fixed;
+  right: 0;
+  top:0;
+  padding: 20px;
+  box-sizing: border-box;
+  background-color: #3c3b42;
+  border-bottom: 4px solid #3d7cc6;
+  box-shadow: 0px 1px 7px 5px #4a4a4a;
+  color: white;
+  width: 100%;
+  select {
+    width: 100%;
+    background-color: #53545f;
+    color: white;
+    border: none;
+    outline: none;
+    padding: 5px;
+    margin-top: 10px;
+  }
+}
+</style>
--- gasoil/src/views/Home.vue
+++ gasoil/src/views/Home.vue
@@ -7,12 +7,7 @@
       @markerClick="markerClick"
       >
     </map-cmp>
-    <div class="overlay">
-      <div>Type de carburant:</div>
-      <select :value="Stations.carburant" @input="Stations.setCarburant($event.target.value)">
-        <option :value="carburant.id" v-for="carburant of Stations.carburants" :key="carburant.id">{{carburant.label}}</option>
-      </select>
-    </div>
+    <fuel-overlay></fuel-overlay>
     <transition name="fade">
       <div class="station" v-if="selectedStation" :key="selectedStation.data.$.latitude + selectedStation.data.$.longitude">
         <section class="title">
@@ -74,12 +69,14 @@ import MapVue from '../components/Map.vue';
 import PromiseB from 'bluebird'
 import Stations from '../services/stations'
 import position from '../services/position'
+import FuelOverlayVue from '../components/FuelOverlay.vue';
 moment.locale('fr')
 
 export default {
   name: 'Home',
   components: {
-    mapCmp: MapVue
+    mapCmp: MapVue,
+    fuelOverlay: FuelOverlayVue
   },
   data() {
     return {
@@ -151,28 +148,6 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-.overlay {
-  position: fixed;
-  right: 0;
-  top:0;
-  padding: 20px;
-  box-sizing: border-box;
-  background-color: #3c3b42;
-  border-bottom: 4px solid #3d7cc6;
-  box-shadow: 0px 1px 7px 5px #4a4a4a;
-  color: white;
-  width: 100%;
-  select {
-    width: 100%;
-    background-color: #53545f;
-    color: white;
-    border: none;
-    outline: none;
-    padding: 5px;
-    margin-top: 10px;
-  }
-}
-
 .station {
   position: fixed;
   right: 0;
